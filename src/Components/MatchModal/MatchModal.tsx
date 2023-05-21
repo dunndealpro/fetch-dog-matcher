ab6fec47ffd8616a41000fa4215c7d92ca49fd41
dunndealpro <dunn.evan.f@gmail.com>
2023-05-20 21:37:44 -0700

    update active state of matched button

--- src/Components/MatchModal/MatchModal.tsx
+++ src/Components/MatchModal/MatchModal.tsx
@@ -24,18 +24,10 @@ interface MatchModalProps{
 
 const MatchModal: FC<MatchModalProps> = (props) =>{
 
-    console.log("match! ", props.match)
-
-//     const [matchInfo, setMatchInfo] = useState<Dog>()
-
-    // const matchInfoUrl = `https://frontend-take-home-service.fetch.com/dogs`;
-    // const reqBodyParams = props.match;  
-
     const matchInfoUrl = `https://frontend-take-home-service.fetch.com/dogs`;
     const [matchInfo, setMatchInfo] = useState<Dog>()
 
     async function getMatchInfo() {
-        console.log(props.match)
         if(props.match){
         const reqBodyParams = [props.match?.match]
         const info = await fetch(matchInfoUrl, {
@@ -46,7 +38,6 @@ const MatchModal: FC<MatchModalProps> = (props) =>{
           },
           body: JSON.stringify(reqBodyParams),
         }).then((res) => res.json());
-        console.log(info);
         setMatchInfo(info[0]);
         }
       }
@@ -74,9 +65,7 @@ useEffect(()=>{
         <div>Age: <strong>{matchInfo && matchInfo?.age}</strong></div>
         <div>Current Location: (feature coming soon)</div>
       </Modal.Body>
-      {/* <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer> */}
+    
     </Modal>
         </>
     )
--- src/Components/SearchResultItem/SearchResultItem.tsx
+++ src/Components/SearchResultItem/SearchResultItem.tsx
@@ -4,18 +4,17 @@ import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap";
 import { Accordion } from "react-bootstrap";
 import { Container, Col, Row } from "react-bootstrap";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 
 import "./SearchResultItem.css";
 
-interface LikedDogs{
-  likedDogs: string[] | undefined
+interface LikedDogs {
+  likedDogs: string[] | undefined;
 }
 
 // interface LikedDog{
 //   likedDog: string
 // }
-
 
 interface Dog {
   id: string;
@@ -36,32 +35,31 @@ interface SearchResult {
 interface SearchResultItemProps {
   dog: string;
   searchResult: SearchResult;
-  likedDogs?: LikedDogs
+  likedDogs?: LikedDogs;
   setLikedDogs: React.Dispatch<React.SetStateAction<LikedDogs | undefined>>;
 }
 
 const SearchResultItem: FC<SearchResultItemProps> = (props) => {
   const [dogInfo, setDogInfo] = useState<Dog>();
-  const [display, setDisplay] = useState("I am not sure...")
-  const [isLiked, setIsLiked] = useState(false)
+  const [display, setDisplay] = useState("I am not sure...");
+  const [isLiked, setIsLiked] = useState(false);
 
   let dogSearchUrl = `https://frontend-take-home-service.fetch.com/dogs`;
 
-  const reqBodyParams = [props.dog];  
-  
-  function checkForLikes(dogId: string | undefined){
-    if( props.likedDogs === undefined){
-      setIsLiked(false)      
-    }else{
-      const likedDogTemp = props.likedDogs.likedDogs
-      const idx = likedDogTemp?.indexOf(dogId || '')
-      if(idx !== undefined && idx > -1){
-        setIsLiked(true)
-      }else{
-        setIsLiked(false)
-        
+  const reqBodyParams = [props.dog];
+
+  function checkForLikes(dogId: string | undefined) {
+    if (props.likedDogs === undefined) {
+      setIsLiked(false);
+    } else {
+      const likedDogTemp = props.likedDogs.likedDogs;
+      const idx = likedDogTemp?.indexOf(dogId || "");
+      if (idx !== undefined && idx > -1) {
+        setIsLiked(true);
+      } else {
+        setIsLiked(false);
       }
-  }
+    }
   }
 
   async function getDog() {
@@ -73,72 +71,71 @@ const SearchResultItem: FC<SearchResultItemProps> = (props) => {
       },
       body: JSON.stringify(reqBodyParams),
     }).then((res) => res.json());
-    setDogInfo(dogResults[0])
-    checkForLikes(dogResults[0].id)
+    setDogInfo(dogResults[0]);
+    checkForLikes(dogResults[0].id);
   }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-   
-   let temp = props.likedDogs?.likedDogs
-   if(e.target.checked){
-    setDisplay("I like this doggo!")
-    setIsLiked(true)
-    if(temp === undefined){
-      let tempNew = [e.target.id]
-      props.setLikedDogs({likedDogs: tempNew})
+    let temp = props.likedDogs?.likedDogs;
+    if (e.target.checked) {
+      setDisplay("I like this doggo!");
+      setIsLiked(true);
+      if (temp === undefined) {
+        let tempNew = [e.target.id];
+        props.setLikedDogs({ likedDogs: tempNew });
+      }
+      if (temp) {
+        temp.push(e.target.id);
+        props.setLikedDogs({ likedDogs: temp });
+      }
+    } else {
+      setDisplay("I am not sure...");
+      setIsLiked(false);
+      let idx = temp?.indexOf(e.target.id);
+      if (idx === undefined || idx === -1) {
+        return;
+      } else {
+        temp?.splice(idx, 1);
+        props.setLikedDogs({ likedDogs: temp });
+      }
     }
-    if (temp){
-      temp.push(e.target.id)
-      props.setLikedDogs({likedDogs: temp})
-    }    
-   }else{
-    setDisplay("I am not sure...")
-    setIsLiked(false)
-    let idx = temp?.indexOf(e.target.id)
-    if(idx=== undefined || idx === -1){
-      return
-    }else{
-      temp?.splice(idx, 1)
-      props.setLikedDogs({likedDogs: temp})
-    }
-   }
-}
+  };
 
   useEffect(() => {
     getDog();
-    
   }, [props.searchResult, props.likedDogs, isLiked]);
 
-  console.log(props.likedDogs)
+  console.log(props.likedDogs);
 
   return (
     <>
-      <Card>
-      
-        <Card.Img variant="top" src={dogInfo?.img} />
+      <Card className="">
+        <Card.Text className="p-2 bg-secondary">{dogInfo?.breed}</Card.Text>
+        <Card.Img variant="bot" src={dogInfo?.img} />
         <Card.Body>
-          <Accordion >
+          <Accordion>
             <Accordion.Item eventKey="0">
               <Accordion.Header>Meet {dogInfo?.name}</Accordion.Header>
               <Accordion.Body>
                 <Card.Text>
-                  Hi, I am a {dogInfo?.age} year old {dogInfo?.breed} and I am
-                  currently located in {dogInfo?.zip_code}
+                  <div>Age: <strong>{dogInfo?.age}</strong></div>
+                  <div>Location: <strong>{dogInfo?.zip_code}</strong></div>
+
+                  {/* Hi, I am a {dogInfo?.age} year old {dogInfo?.breed} and I am
+                  currently located in {dogInfo?.zip_code} */}
                 </Card.Text>
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
-          {/* <Card.Text>
-            Hi, I am {dogInfo?.age} years old {dogInfo?.breed} and I am currently located in {dogInfo?.zip_code}
-          </Card.Text> */}
-          {/* <Button variant="primary">Go somewhere</Button> */}
-          <Form.Check className="mt-3"
-        type="switch"
-        id={dogInfo?.id}
-        label={display}
-        checked={isLiked}
-        onChange={e => handleChange(e)}
-      />
+
+          <Form.Check
+            className="mt-3"
+            type="switch"
+            id={dogInfo?.id}
+            label={display}
+            checked={isLiked}
+            onChange={(e) => handleChange(e)}
+          />
         </Card.Body>
       </Card>
     </>
