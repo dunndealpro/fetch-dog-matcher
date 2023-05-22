86c7fd0cb53fbd67b86f29fa4b632de319b46eb4
dunndealpro <dunn.evan.f@gmail.com>
2023-05-22 09:37:00 -0700

    update index file for deployment

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
 import App from "./Pages/App/App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -21,4 +21,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
